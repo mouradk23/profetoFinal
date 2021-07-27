ba585dcb1a583fa2056f78228f9587bfe8fce8da
mouradk23 <wendelvsm@gmail.com>
2021-07-27 15:19:45 -0300

    substituí registro user po rchave

--- rotas/pageRouters.js
+++ rotas/pageRouters.js
@@ -5,8 +5,12 @@ router.get('/login', (req, res) => {
     res.render('../pages/login')
 })
 
-router.get('/registrousuario', (req, res) => {
-    res.render('../pages/registroUsuario')
+// router.get('/registrousuario', (req, res) => {
+//     res.render('../pages/registroUsuario')
+// })
+
+router.get('/registrochave', (req, res) => {
+    res.render('../pages/registroChave')
 })
 
 router.get('/chaves', (req, res) => {
