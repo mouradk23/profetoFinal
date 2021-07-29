98fa484d4f69e9619bc17cfdd4f90846a0ddb7e2
mouradk23 <wendelvsm@gmail.com>
2021-07-29 15:52:53 -0300

    funcao de receber chave e muda a disponibilidade da chave

--- controller/emprestimeController.js
+++ controller/emprestimeController.js
@@ -1,4 +1,5 @@
 const Sequelize =  require('sequelize')
+const Chave = require('../models/Chaves')
 const Emprestimo = require('../models/Emprestimo')
 
 
@@ -6,13 +7,30 @@ module.exports = {
     async emprestar(req, res) {
         try {
             const { status, pessoa, cpf, chave_id } = req.body 
-            const novoEmprestimo = await Emprestimo.create({status, pessoa, cpf, chave_id })
-
+            const chave = await Chave.findByPk(chave_id)
+            console.log(req.body);
+            const novoEmprestimo = await Emprestimo.create({status, pessoa, cpf })
+            await chave.update({disponibilidade: true})
+            await novoEmprestimo.setChave(chave)
             if (novoEmprestimo) {
                 res.status(201).json({emprestimo: novoEmprestimo, created: true});
             }
         } catch (error) {
             res.status(400).json(error)
         }
+    },
+    async atualizar(req, res) {
+        try {
+            const { id, status, chave_id } = req.body
+            const chave = await Chave.findByPk(chave_id)
+            console.log(req.body);
+            const emprestimo = await Emprestimo.findByPk(id)
+            console.log(emprestimo);
+            await chave.update({disponibilidade: false})
+            await emprestimo.update({status: true})
+            res.status(200).json(emprestimo)
+        } catch (error) {
+            res.status(400).json(error)
+        } 
     }
 }
